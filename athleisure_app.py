54c36c3287e88377ec65d352ce4d014ca1bfbf6b
mba58 <161256536+mba58@users.noreply.github.com>
2024-09-02 19:40:26 +0200

    Commit LP36131 2028-09-02a.

--- athleisure_app.py
+++ athleisure_app.py
@@ -1,49 +1,34 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
+#from snowflake.snowpark.context import get_active_session
+
 from snowflake.snowpark.functions import col
 
+#session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
 
+
 st.title("ZENA's Amazing Athleisure Catalog")
 
-colors_df = session.table("ZENAS_ATHLEISURE_DB.PRODUCTS.SWEATSUITS").select(col('COLOR_OR_STYLE'), col('PRICE'))
+colors_df = session.table("ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE").select(col('COLOR_OR_STYLE'), col('PRICE'), col('FILE_URL'), col('SIZE_LIST'), col('UPSELL_PRODUCT_DESC'))
 st.dataframe(data=colors_df, use_container_width=True)
 
-st.selectbox('Pick a sweatsuit color or style:', 
+option = st.selectbox('Pick a sweatsuit color or style:', 
              colors_df
          )
+st.write('-- Step 3: '+option)
 
+choice_df = colors_df.filter((col("COLOR_OR_STYLE") == option))
+choice_df.show()
+#price_df = choice_df.select(col('PRICE'))
+price = choice_df.select(col('PRICE')).to_pandas().iloc[0]["PRICE"]
+st.write('Price: '+str(price))
+#url = choice_df.select(col('FILE_URL')).to_pandas().iloc[0]["FILE_URL"]
+#st.write('URL '+str(url))
+size_list = choice_df.select(col('SIZE_LIST')).to_pandas().iloc[0]["SIZE_LIST"]
+st.write('Sizes available:  '+str(size_list))
+upsell = choice_df.select(col('UPSELL_PRODUCT_DESC')).to_pandas().iloc[0]["UPSELL_PRODUCT_DESC"]
+st.write(upsell)
 
-# name_on_order = st.text_input('Name on Smoothie:')
-
-# session = get_active_session()
-# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# #st.dataframe(data=my_dataframe, use_container_width=True)
-
-# ingredients_list = st.multiselect(
-    # 'Cooose up to 5 ingredients: '
-    # , my_dataframe
-    # , max_selections = 5
-# )
-
-# if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
-
-    # ingredients_string = ''
-    # for f in ingredients_list:
-        # ingredients_string += f + ' '
-
-    # #st.write(ingredients_string)
-    
-    # time_to_insert = st.button('Submit Order')
-    # if time_to_insert:
-        # my_insert_stmt = """ insert into smoothies.public.orders(NAME_ON_ORDER, INGREDIENTS)
-            # values ('""" + name_on_order + """', '"""+ ingredients_string + """')"""
-        # # st.write(my_insert_stmt)
-        # # st.stop()
-
-        # if ingredients_string:
-            # session.sql(my_insert_stmt).collect()
-            # st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
